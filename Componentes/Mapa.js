09585c3aaea4b8caf02cfbf59d0a66848bc3f93b
Fabi <fabianagomes0829@gmail.com>
2023-12-04 17:43:45 -0300

    04/12

--- Componentes/Mapa.js
+++ Componentes/Mapa.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import MapView, { Marker } from 'react-native-maps';
-import { StyleSheet, View } from 'react-native';
+import { Image } from 'react-native';
+import { StyleSheet, View, TouchableOpacity, Text } from 'react-native';
 import { requestForegroundPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 
-export default function ScreenLogin() {
+export default function Mapa() {
   const [initialRegion, setInitialRegion] = useState(null);
+  const [overlayVisible, setOverlayVisible] = useState(false);
 
   useEffect(() => {
     const getLocation = async () => {
@@ -26,20 +28,36 @@ export default function ScreenLogin() {
     getLocation();
   }, []);
 
+  const handleOverlayToggle = () => {
+    setOverlayVisible(!overlayVisible);
+  };
+
   return (
-    <View style={styles.container}>
-      {initialRegion && (
+      <View style={styles.container}>
         <MapView
           style={styles.map}
           initialRegion={initialRegion}
           showsUserLocation={true}
           followsUserLocation={true}
         >
-          <Marker coordinate={{ latitude: initialRegion.latitude, longitude: initialRegion.longitude }} />
+          {initialRegion && (
+            <Marker coordinate={{ latitude: initialRegion.latitude, longitude: initialRegion.longitude }} />
+          )}
         </MapView>
-      )}
-    </View>
-  );
+    
+        <TouchableOpacity style={styles.floatingButton} onPress={handleOverlayToggle}>
+          <Text style={styles.buttonText}>{overlayVisible ? 'Fechar' : 'Abrir'}</Text>
+        </TouchableOpacity>
+    
+        {overlayVisible && (
+          <View style={styles.overlayContainer}>
+            <View style={styles.overlayContent}>
+              <Text style={styles.overlayText}>Sobreposição com desfoque</Text>
+            </View>
+          </View>
+        )}
+      </View>
+    );
 }
 
 const styles = StyleSheet.create({
@@ -47,6 +65,37 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   map: {
-    flex: 1,
+    ...StyleSheet.absoluteFillObject,
+  },
+  overlayContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: '90%',
+    justifyContent: 'flex-end',
+  },
+
+  overlayContent: {
+    height: '80%',
+    backgroundColor: 'rgba(255, 255, 255, 0.8)', // Ajuste o valor do último componente (0.5) para a opacidade desejada
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  overlayText: {
+    fontSize: 18,
+    color: 'black',
+  },
+  floatingButton: {
+    position: 'absolute',
+    top: '20%',  // Ajustado para manter o botão a 60% da página
+    right: 20,
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 1,
   },
 });
--- Componentes/Navigation.js
+++ Componentes/Navigation.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { GestureHandlerRootView } from 'react-native-gesture-handler'; // Importe o GestureHandlerRootView
 import ScreenOne from './ScreenOne';
 import ScreenLogin from './ScreenLogin';
 import Mapa from './Mapa';
@@ -9,15 +10,18 @@ const Stack = createNativeStackNavigator();
 
 const Navigation = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Splash"
-          component={ScreenOne}/>
-        <Stack.Screen name="Login" component={ScreenLogin} />
-        <Stack.Screen name="Mapa" component={Mapa} /> 
-      </Stack.Navigator>
-    </NavigationContainer>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Splash"
+            component={ScreenOne}
+          />
+          <Stack.Screen name="Login" component={ScreenLogin} />
+          <Stack.Screen name="Mapa" component={Mapa} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 };
 
